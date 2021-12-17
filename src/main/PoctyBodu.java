964d4f207b289d32772eb88d2e81ac1f07fec56c
robert <robert.hak@tul.cz>
2021-12-17 11:52:51 +0100

    cum

--- src/main/PoctyBodu.java
+++ src/main/PoctyBodu.java
@@ -11,12 +11,12 @@ public class PoctyBodu {
         int n = s.nextInt();
         int[] kvadrantyPocet = new int[4];
         for (int i=0; i < n; i++){
-            double input1 = s.nextDouble();
-            double input2 = s.nextDouble();
-            if(input1 >= zero && input2 >= zero) kvadrantyPocet[0]++;
-            if(input1 < zero && input2 >= zero) kvadrantyPocet[1]++;
-            if(input1 <= zero && input2 < zero) kvadrantyPocet[2]++;
-            if(input1 > zero && input2 < zero) kvadrantyPocet[3]++;
+            double x = s.nextDouble();
+            double y = s.nextDouble();
+            if(x >= zero && y >= zero) kvadrantyPocet[0]++;
+            if(x < zero && y >= zero) kvadrantyPocet[1]++;
+            if(x <= zero && y < zero) kvadrantyPocet[2]++;
+            if(x > zero && y < zero) kvadrantyPocet[3]++;
         }
         System.out.printf("%d %d %d %d%n", kvadrantyPocet[0], kvadrantyPocet[1], kvadrantyPocet[2], kvadrantyPocet[3]);
     }
