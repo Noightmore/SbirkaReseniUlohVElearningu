c75bfb1565c56cd68823468b4706762f96ac0068
Noightmore <robert.hak@tul.cz>
2022-01-12 19:35:07 +0100

    updates updates...

--- src/main/RozmeryPlochy.java
+++ src/main/RozmeryPlochy.java
@@ -1,6 +1,5 @@
 package main;
 
-
 import java.util.Scanner;
 
 public class RozmeryPlochy {
@@ -9,26 +8,40 @@ public class RozmeryPlochy {
     public static void main(String[] args) {
         int n = sc.nextInt();
         for(int i = 0; i < n; i++){
-            int hMax = 1;
-            int lMax = 1;
+            int hMax = 0;
+            int lrMax = 0;
+            int llMax = 0;
+            int dMax = 0;
             int x = 0;
             int y = 0;
-            String[] moves = sc.next().split("");
+
+            StringBuilder line = new StringBuilder();
+            do {
+                line.append(sc.nextLine());
+            } while (!line.toString().contains("E"));
+
+            String[] moves = line.toString().split("");
             for (String move : moves) {
                 switch (move){
                     case "S":
                         y++;
+                        if(hMax < y) hMax = y;
+                        break;
                     case "J":
                         y--;
+                        if(dMax > y) dMax = y;
+                        break;
                     case "V":
                         x++;
+                        if(lrMax < x) lrMax = x;
+                        break;
                     case "Z":
                         x--;
+                        if(llMax > x) llMax = x;
+                        break;
                 }
-                if(hMax < Math.abs(y)) hMax = Math.abs(y);
-                if(lMax < Math.abs(x)) lMax = Math.abs(x);
             }
-            System.out.printf("%d %d%n", hMax, lMax);
+            System.out.printf("%d %d%n",  lrMax + (-1*llMax) + 1, hMax + (-1*dMax) + 1 );
         }
     }
 }
